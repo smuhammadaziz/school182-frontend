f30da721b7d2cc56063cd272b1dc38764b522006
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2023-03-04 16:22:01 +0500

    add: add statistics section

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,6 +5,7 @@ import Header from '../../components/header/header';
 import Intro from '../../components/intro/intro';
 import Administration from '../../components/administration/administration';
 import News from '../../components/news/news';
+import Statistics from '../../components/statistics/statistics';
 
 function Home() {
   return (
@@ -17,6 +18,7 @@ function Home() {
         <Intro />
         <Administration />
         <News />
+        <Statistics />
       </main>
     </div>
   );
