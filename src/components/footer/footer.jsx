6842139f788a5c0a81af072da580ca278919d23e
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2023-09-06 22:08:23 +0500

    edit: change footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -12,16 +12,30 @@ const Footer = () => {
   const [lang] = useLang('ru');
   return (
     <footer>
-      <div className='container'>
+      <div className='container footer-container'>
         <div className='neo'>
           <img src={logo} className='brand' />
+
+          <p className='site_desc'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ea quidem
+            aliquid, repellendus ad minima voluptatum ex facilis eos debitis!
+          </p>
         </div>
 
         <div className='mesto'>
           <h5>{content[lang].footer.location}</h5>
-          <pre>
-            г. Ташкент, <br /> Мирабадский район, <br /> проспект Амира <br /> темура 33
-          </pre>
+          <div className='col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 mesto_inline'>
+            <p>
+              <i className='bi bi-house-door-fill'></i> New York, NY 10012, US
+            </p>
+            <p>
+              <i className='bi bi-envelope-fill'></i>
+              info@example.com
+            </p>
+            <p>
+              <i className='bi bi-telephone-fill'></i> + 01 234 567 88
+            </p>
+          </div>
         </div>
         <ul className='sotsial'>
           <h5>{content[lang].footer.social}</h5>
@@ -40,18 +54,14 @@ const Footer = () => {
         </ul>
       </div>
 
-      <div className='footer__down' style={{ display: 'flex', flexDirection: 'column' }}>
+      {/* <div className='footer__down'>
         <a href='#' className='footer__down__link'>
           {content[lang].neosoft.made}
         </a>
-        <a
-          href='tel:+998900291088'
-          className='footer__down__link'
-          style={{ marginTop: '10px' }}
-        >
+        <a href='tel:+998900291088' className='footer__down__link'>
           Тел: +998 (90) 029 10 88
         </a>
-      </div>
+      </div> */}
     </footer>
   );
 };
