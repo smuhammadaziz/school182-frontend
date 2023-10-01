440da9350bb0c81cce618814ab58b2f050e0ce14
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2023-10-01 17:02:37 +0500

    add: add gallery page

---
+++ src/components/gallery/gallery.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './gallery.scss';
+import { Gallery } from 'react-grid-gallery';
+
+import { images as IMAGES } from '../../database/images';
+
+const images = IMAGES.map(image => ({
+  ...image,
+  customOverlay: (
+    <div className='custom-overlay__caption'>
+      <div>{image.caption}</div>
+      {image.tags &&
+        image.tags.map((t, index) => (
+          <div key={index} className='custom-overlay__tag'>
+            {t.title}
+          </div>
+        ))}
+    </div>
+  ),
+}));
+
+function Galleries() {
+  return (
+    <div className='galleries'>
+      <div className='container'>
+        <div>
+          <Gallery images={images} enableImageSelection={false} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Galleries;
--- src/pages/gallery/gallery.jsx
+++ src/pages/gallery/gallery.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import './gallery.scss';
+
+import Header from '../../components/header/header';
+import Footer from '../../components/footer/footer';
+import Galleries from '../../components/gallery/gallery';
 
 function Gallery() {
-  return <div>Gallery</div>;
+  return (
+    <div className='gallery'>
+      <header>
+        <Header />
+      </header>
+
+      <main>
+        <Galleries />
+      </main>
+
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
 
 export default Gallery;
